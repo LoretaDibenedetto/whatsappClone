882c7c33dc1a3705ad79fea04e69bc701f96879e
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-09-07 15:41:55 +0200

    add flex in sidebar

--- src/sidebar/Sidebar.js
+++ src/sidebar/Sidebar.js
@@ -1,12 +1,23 @@
-import React from 'react'
+import React from 'react';
+import "./Sidebar.css";
 
 function Sidebar() {
   return (
-    <div>
-        <h1>
-            Sidebar
-        </h1>
+    <div className='sidebar'>
+     <div className='sidebar_header'>
+      <div className='sidebar_header_left'>
+        <h1 > header  1</h1>
     </div>
+     <div className='sidebar_header_right'>
+       <h1 > header 2 </h1>
+
+
+
+      </div>
+     </div>
+ </div>
+
+
   )
 }
 
