eeb89a4fb85023a69576df93210d62535ff2e028
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-09-08 15:17:46 +0200

    add all icon  in header

--- src/sidebar/Sidebar.js
+++ src/sidebar/Sidebar.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import "./Sidebar.css";
 import ChatIcon from '@mui/icons-material/Chat';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import DonutLargeIcon from '@mui/icons-material/DonutLarge';
 
+import {Avatar, IconButton} from "@mui/material"
 
 function Sidebar() {
   return (
     <div className='sidebar'>
      <div className='sidebar_header'>
       <div className='sidebar_header_left'>
-        <h1 > header  1</h1>
+        <IconButton> <Avatar src="https://w7.pngwing.com/pngs/340/946/png-transparent-avatar-user-computer-icons-software-developer-avatar-child-face-heroes.png"></Avatar></IconButton>
+       
     </div>
      <div className='sidebar_header_right'>
-       <h1 > header 2 </h1>
-
-      <ChatIcon></ChatIcon>
-
+  
+      <IconButton>
+        <DonutLargeIcon></DonutLargeIcon>
+      </IconButton>
+      <IconButton>
+        <ChatIcon></ChatIcon>
+      </IconButton>
+      <IconButton>
+        <MoreVertIcon></MoreVertIcon>
+      </IconButton>
       </div>
      </div>
  </div>
