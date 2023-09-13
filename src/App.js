0bbbb302daa994a9c8ab6258e87ad6c270279651
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-09-13 12:07:50 +0200

    configure axios

--- src/App.js
+++ src/App.js
@@ -3,20 +3,36 @@ import './App.css';
 import Sidebar from './sidebar/Sidebar';
 import Chat from './chat/Chat';
 import Pusher from "pusher-js"
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { cleanup } from '@testing-library/react';
+import axios from "./axios";
 
 function App() {
- 
+  const [count, setCount] = useState(56);
+  const [messages, setMessages] = useState([]);
+
+ useEffect(()=>{
+  axios.get("/api/v1/messages/sync")
+  .then((response)=>{
+    console.log(response.data);
+  }).catch();
+})
+
+
   useEffect(() =>{
     var pusher = new Pusher('4a923e7496e850c7e247', {
       cluster: 'eu'
     });
 
     var channel = pusher.subscribe('messages');
-    channel.bind('inserted', function(data) {
-      alert(JSON.stringify(data));
+    channel.bind('inserted', function(newMessage) {
+     setMessages([...messages, newMessage]);
     });
+    
+    return ()=>{
+      channel.unbind_all();
+      channel.unsubscribe();
+    }
 
     console.log("effect app");
   });
@@ -24,6 +40,7 @@ function App() {
 
   return (
     <div className="app">
+  
       <div className="app_body">
       <Sidebar></Sidebar>
       <Chat></Chat>
