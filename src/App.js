e6cebbdc081b6d479e88d9a6276da4e36813b238
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-09-13 11:15:32 +0200

    add pusher

--- src/App.js
+++ src/App.js
@@ -2,8 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 import Sidebar from './sidebar/Sidebar';
 import Chat from './chat/Chat';
+import Pusher from "pusher-js"
+import { useEffect } from 'react';
+import { cleanup } from '@testing-library/react';
 
 function App() {
+ 
+  useEffect(() =>{
+    var pusher = new Pusher('4a923e7496e850c7e247', {
+      cluster: 'eu'
+    });
+
+    var channel = pusher.subscribe('messages');
+    channel.bind('inserted', function(data) {
+      alert(JSON.stringify(data));
+    });
+
+    console.log("effect app");
+  });
+
+
   return (
     <div className="app">
       <div className="app_body">
