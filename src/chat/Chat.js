910edbfbb6394ce4d5bcf71c5e7c16ccc96589e2
LoretaDibenedetto <dibenedettoloreta@gmail.com>
2023-09-14 09:50:57 +0200

    add create in chat component

--- src/chat/Chat.js
+++ src/chat/Chat.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import axios from "../axios";
+import { useEffect, useState } from 'react';
 import "./Chat.css";
 import { Avatar, Button, IconButton } from '@mui/material';
 import { AttachFile, EmojiEmotions, MoreVert, SearchOffOutlined, SearchOutlined } from '@mui/icons-material';
 
 function Chat({ messages }) {
+  const [input,setInput] = useState("");
+  const sendMessage = async (e) =>{
+    e.preventDefault();
+    await axios.post("/api/v1/messages",{
+      message : input,
+      name : "Author Name",
+      timestamp :"now",
+      received:false
+    }).then();
+    setInput("");
+  }
   return (
     <div className='chat'>
         <div className='chat_header'>
@@ -32,7 +45,7 @@ function Chat({ messages }) {
         <div className='chat_body'>
         {messages.map((message) => {
         return (
-                <p className={`chat_message ${message.received && "chat_recever"} `}>
+                <p key={message._id}className={`chat_message ${message.received && "chat_recever"} `}>
                   <span className='chat_name'>{message.name}</span>
                   {message.message} {/* Utilizza la proprietà 'text' del messaggio qui */}
                   <span className='chat_timestamp'>{message.timestamp}</span>
@@ -45,8 +58,10 @@ function Chat({ messages }) {
           <EmojiEmotions></EmojiEmotions>
         </IconButton>
         <form>
-          <input placeholder='scrivi un messaggio..' type="text"></input>
-          <Button type='submit'></Button>
+          <input value={input} 
+          onChange={(e) => setInput(e.target.value)}
+          placeholder='scrivi un messaggio..' type="text"></input>
+          <Button onClick={sendMessage} type='submit'></Button>
         </form>
         </div>
     </div>
